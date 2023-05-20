976d61aed26a98db41ba3ad579e9a1a7476de916
ahmed1812 <algandel19@gmail.com>
2023-05-20 16:31:17 -0500

    add model

---
+++ BulkyWorkWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace BulkyWorkWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string JobTitel { get; set; }
+        public DateTime DateOfSubmit { get; set; } = DateTime.Now;
+        public string PhoneInterView { get; set; }
+        public string VideoInterView { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+    }
+}
