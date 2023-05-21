622ecb4b493ade61b67a8800986a66074c14a72a
ahmed1812 <algandel19@gmail.com>
2023-05-21 13:22:25 -0500

    data run

---
+++ BulkyWorkWeb/Migrations/20230521181703_work.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWorkWeb.Migrations
+{
+    public partial class work : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobTitel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfSubmit = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneInterView = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VideoInterView = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
