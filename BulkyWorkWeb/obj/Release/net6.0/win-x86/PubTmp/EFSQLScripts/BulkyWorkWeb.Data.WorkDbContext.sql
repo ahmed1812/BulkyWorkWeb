99e0ae3fae1ae0ecf30a8763d38e62c3f8d90be0
ahmed1812 <algandel19@gmail.com>
2023-06-04 14:12:58 -0500

    run 64

---
+++ BulkyWorkWeb/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/BulkyWorkWeb.Data.WorkDbContext.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230521181703_work')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [CompanyName] nvarchar(max) NOT NULL,
+        [Address] nvarchar(max) NOT NULL,
+        [JobTitel] nvarchar(max) NOT NULL,
+        [DateOfSubmit] datetime2 NOT NULL,
+        [PhoneInterView] nvarchar(max) NOT NULL,
+        [VideoInterView] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        [Note] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230521181703_work')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230521181703_work', N'6.0.0');
+END;
+GO
+
+COMMIT;
+GO
+
